9c52674d51654eab417714ebcaa0fbab9a9182c5
jose <jmrodriguez@fi.uba.ar>
2018-11-06 09:39:48 -0300

    Se agregan pruebas a Coordenadas

--- test/fiuba/algo3/aoe/CoordenadaTest.java
+++ test/fiuba/algo3/aoe/CoordenadaTest.java
@@ -23,10 +23,27 @@ public class CoordenadaTest {
     public void test02DosCoordenadasConLasMismasComponentesSonLaMismaCoordenada() {
         Coordenada coordenada1 = new Coordenada( 2, 2 );
         Coordenada coordenada2 = new Coordenada( 2, 2 );
-        Coordenada coordenada3 = new Coordenada( 3, 3 );
+        Coordenada coordenada3 = new Coordenada( 3, 2 );  // != x
+        Coordenada coordenada4 = new Coordenada( 2, 3 );  // != y
 
         Assert.assertThat(coordenada1, is(coordenada2 ) );
         Assert.assertThat(coordenada1, is( not( coordenada3 ) ) );
+        Assert.assertThat(coordenada1, is( not( coordenada4 ) ) );
     }
+
+    @Test
+    public void test03EqualsDeUnaMismaCoordenadaDebeDarTrue() {
+        Coordenada coordenada1 = new Coordenada( 2, 2 );
+
+
+        Assert.assertTrue(coordenada1.equals(coordenada1));
+    }
+
+    @Test
+    public void test04EqualsDeUnaCoordenadaConNullDebeDarFalse() {
+        Coordenada coordenada1 = new Coordenada( 2, 2 );
+        Assert.assertFalse(coordenada1.equals(null));
+    }
+
 
 }
