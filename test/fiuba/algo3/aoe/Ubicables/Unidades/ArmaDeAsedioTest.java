fa0bf4df0281daf6d2cfc7cd3f4c5cdb604519b2
jose <jmrodriguez@fi.uba.ar>
2018-11-10 17:53:17 -0300

    Unos Test para mirar

--- test/fiuba/algo3/aoe/Ubicables/Unidades/ArmaDeAsedioTest.java
+++ test/fiuba/algo3/aoe/Ubicables/Unidades/ArmaDeAsedioTest.java
@@ -1,7 +1,14 @@
 package fiuba.algo3.aoe.Ubicables.Unidades;
+import fiuba.algo3.aoe.Tablero.Tablero;
+import fiuba.algo3.aoe.Ubicables.Direccion.DireccionDerecha;
+import fiuba.algo3.aoe.Ubicables.Direccion.Direccionable;
+import fiuba.algo3.aoe.Ubicables.Ubicable;
 import fiuba.algo3.aoe.Ubicables.Unidades.ArmaDeAsedio;
+import fiuba.algo3.aoe.Ubicables.posicion.Posicion;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 
 public class ArmaDeAsedioTest {
 
@@ -32,4 +39,43 @@ public class ArmaDeAsedioTest {
 
     }
 
+
+    @Test
+    public void test200AlColocarUnaPiezaEnelTableroCambiaLaPosicionDeLaPiezaYEnElTablero(){
+        Tablero tablero = new Tablero(10,10);
+        Ubicable lanzaPiedas = new ArmaDeAsedio();
+        Posicion origen = new Posicion(2,5);
+
+        tablero.colocar(lanzaPiedas,origen);
+
+
+        Assert.assertThat(tablero.puedoColocar(origen), is(false) );
+        Assert.assertThat(lanzaPiedas.getPosicion(), is(origen) );
+
+
+    }
+
+    /*
+    @Test
+    public void test201MoverCambiaLaPosicionEnElTableroYLaUnidadQuedaConLaNuevaPosicion(){
+        Tablero tablero = new Tablero(10,10);
+        Ubicable lanzaPiedas = new ArmaDeAsedio();
+        Direccionable direccion = new DireccionDerecha();
+
+        Posicion origen = new Posicion(2,5);
+        Posicion calculada = origen.calcularPosicionSiguiente(direccion);
+
+        tablero.colocar(lanzaPiedas,origen);
+
+        // le digo que se mueva y le paso la direccion
+        lanzaPiedas.mover(tablero, direccion);
+
+        Assert.assertThat(tablero.puedoColocar(origen), is(false) ); //quedo libre
+        Assert.assertThat(lanzaPiedas.getPosicion(), is(calculada) ); // tiene la nueva posicion
+        Assert.assertThat(tablero.puedoColocar(calculada), is(false) ); // la posicion en el tablero esta ocupada
+        Assert.assertThat(tablero.puedoColocar(origen), is(true) ); // el origen esta libre
+
+
+    }
+*/
 }
