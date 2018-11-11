6494cf5d21930204b2cbbe2235d6dbbf28f3982e
Mauricio <migonzalez@fi.uba.ar>
2018-11-11 18:17:23 -0300

    Se incorpora la posicion a los edificios para poder colocarse y se hace que implementen ubicable

--- src/fiuba/algo3/aoe/Ubicables/Edificios/Edificio.java
+++ src/fiuba/algo3/aoe/Ubicables/Edificios/Edificio.java
@@ -1,14 +1,29 @@
 package fiuba.algo3.aoe.Ubicables.Edificios;
 
-public abstract class Edificio {
+import fiuba.algo3.aoe.Ubicables.Ubicable;
+import fiuba.algo3.aoe.Ubicables.posicion.Posicion;
 
+public abstract class Edificio implements Ubicable {
+    private int costo;
     private int vidaTotal;
     private int vidaActual;
-
+    private Posicion posicion;
 
     public Edificio(int unaVida) {
         vidaTotal = unaVida;
         vidaActual = unaVida;
+    }
+
+    public int costo(){
+        return this.costo;
+    }
+
+    public void colocarEn(Posicion posicion){
+        this.posicion = posicion;
+    }
+
+    public Posicion getPosicion(){
+        return this.posicion;
     }
 
     public int vidaTotal() {
