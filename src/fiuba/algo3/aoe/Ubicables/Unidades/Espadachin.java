991045ce59e5249ec3a0f4a67f35dc34c5870c65
Mauricio <migonzalez@fi.uba.ar>
2018-11-12 17:43:39 -0300

    Modificacion de clases UnidadMovil, y refactor

--- src/fiuba/algo3/aoe/Ubicables/Unidades/Espadachin.java
+++ src/fiuba/algo3/aoe/Ubicables/Unidades/Espadachin.java
@@ -1,12 +1,14 @@
 package fiuba.algo3.aoe.Ubicables.Unidades;
 
+import fiuba.algo3.aoe.Jugadores.Jugador;
+import fiuba.algo3.aoe.Tablero.Tablero;
+import fiuba.algo3.aoe.Ubicables.Direccion.Direccionable;
+import fiuba.algo3.aoe.Ubicables.posicion.Posicion;
+
 public class Espadachin extends UnidadMovil{
-    private int vidaMaxima;
-    private int vidaActual;
-    private int costo;
 
-
-    public Espadachin(){
+    public Espadachin( Jugador jugador ){
+        this.jugador = jugador;
         this.vidaActual = 100;
         this.vidaMaxima = 100;
         this.costo = 50;
@@ -16,13 +18,15 @@ public class Espadachin extends UnidadMovil{
 
     public int getVidaActual(){return this.vidaActual;}
 
-    public int costo(){
-        return this.costo;
-    }
-
-
     public void disminuirVida( int vida){
         this.vidaActual -= vida;
+    }
+
+    public void mover( Tablero tablero, Direccionable direccion){
+        Posicion destino = this.obtenerPosicionDeAvance(direccion);
+        if (tablero.puedoColocar(destino)){
+            tablero.moverElemento(this,destino);
+        }
     }
 
 }
--- src/fiuba/algo3/aoe/Ubicables/Unidades/UnidadMovil.java
+++ src/fiuba/algo3/aoe/Ubicables/Unidades/UnidadMovil.java
@@ -1,17 +1,23 @@
 package fiuba.algo3.aoe.Ubicables.Unidades;
 
 
+import fiuba.algo3.aoe.Jugadores.Jugador;
 import fiuba.algo3.aoe.Tablero.Tablero;
 import fiuba.algo3.aoe.Ubicables.Direccion.Direccionable;
 import fiuba.algo3.aoe.Ubicables.Ubicable;
 import fiuba.algo3.aoe.Ubicables.posicion.Posicion;
 
 public abstract class UnidadMovil implements Ubicable {
-    private Posicion posicion;
-    private String jugador;
+    protected Posicion posicion;
+    protected Jugador jugador;
+    protected int vidaMaxima;
+    protected int costo;
+    protected int vidaActual;
 
 
-    public abstract int costo();
+    public int getCosto(){
+        return this.costo;
+    };
 
     public Posicion getPosicion() {
         return this.posicion;
@@ -26,12 +32,12 @@ public abstract class UnidadMovil implements Ubicable {
         return this.posicion.calcularPosicionSiguiente(direccionable);
     }
 
+    public boolean perteneceAJugador(Jugador jugador){
+        return (this.jugador == jugador);
+    }
+
     //TODO: si no se puede mover por que la posicion esta ocupada, tendria que hacer algo, retornar un bool
     // y usar eso para volvera intentar otra cosa, o enviar un mensaje "ahi no me puedo mover"
-    public void mover( Tablero tablero, Direccionable direccion){
-        Posicion destino = this.obtenerPosicionDeAvance(direccion);
-        if (tablero.puedoColocar(destino)){
-            tablero.moverElemento(this,destino);
-        }
-    }
+    public abstract void mover( Tablero tablero, Direccionable direccion);
+
 }
